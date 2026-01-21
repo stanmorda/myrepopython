d0111787e42f0359e53452cc5dfa37b4caf09547
momentizm <stanislavmordanov@gmail.com>
2026-01-21 17:35:42 +0300

    - start Cat and Bowl game coding

---
+++ урок1911/урок2101/scenes.py
@@ -0,0 +1,51 @@
+import sprites
+import pygame
+
+# --- Классы сцен ---
+class Scene:
+    # обработка событий
+    def handle_events(self, events):
+        pass
+    
+    # обновление спрайтов
+    def update(self):
+        pass
+    
+    # отрисовка всех игровых объектов
+    def draw(self, screen):
+        pass
+    
+class StartScene(Scene):
+    def draw(self, screen):
+        screen.fill((30, 30, 30))
+        font = pygame.font.SysFont('Arial', 32)
+        text = font.render(f"Start (ENTER)", True, (255, 255, 255))
+        screen.blit(text, (100, 200))
+        
+    # обновление спрайтов
+    def update(self):
+        pass
+    
+    # обработка событий
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_KP_ENTER:
+                    return GameScene()
+                
+class GameScene(Scene):
+    def __init__(self):
+        self.cat = sprites.Cat()
+        self.bowl = sprites.Bowl()
+    
+    def draw(self, screen):
+        screen.blit(self.cat.surf, self.cat.rect) 
+        screen.blit(self.bowl.surf, self.bowl.rect) 
+    
+    # обновление спрайтов
+    def update(self):
+        pass
+    
+    # обработка событий
+    def handle_events(self, events):
+        pass
---
+++ урок1911/урок2101/sprites.py
@@ -0,0 +1,32 @@
+import pygame
+
+# создать два спрайта : корм, игрок-кот
+
+class Cat(pygame.sprite.Sprite):
+    def __init__(self):
+        # Вызов базового конструктора
+        super(Cat, self).__init__()
+        
+        # Загрузка изображения, настройка альфа-канал для прозрачности
+        self.surf = pygame.image.load('C:/Users/momentizm/Documents/Среда.Питон/урок1911/урок2101/cat.png').convert_alpha()
+        
+        # Добавление прямоугольника для перемещения объекта
+        self.rect = self.surf.get_rect()
+    
+    def update(self):
+        pass
+        
+        
+class Bowl(pygame.sprite.Sprite):
+    def __init__(self):
+        # Вызов базового конструктора
+        super(Cat, self).__init__()
+        
+        # Загрузка изображения, настройка альфа-канал для прозрачности
+        self.surf = pygame.image.load('C:/Users/momentizm/Documents/Среда.Питон/урок1911/урок2101/bowl.png').convert_alpha()
+        
+        # Добавление прямоугольника для перемещения объекта
+        self.rect = self.surf.get_rect()
+        
+    def update(self):
+        pass
