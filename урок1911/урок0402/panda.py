6825c43ffdf0aebe5e10baa42f8d85dae531c949
momentizm <stanislavmordanov@gmail.com>
2026-02-11 16:55:48 +0300

    - panda

--- урок1911/урок0402/panda.py
+++ урок1911/урок0402/panda.py
@@ -1,12 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
+
+# Define a function to double the value
+def random_value(x):
+    return random.randint(10, 250)
 
 
-df = pd.read_csv('C:/Users/momentizm/Documents/Среда.Питон/урок1911/урок0402/Internet Speed 2022.csv')
-df = df.sort_values('mobile', ascending=False)
-df.hist(column='mobile', bins=10)
-plt.show()
-print(df.head(1))
+df1 = pd.read_csv('C:/Users/momentizm/Documents/Среда.Питон/урок1911/урок0402/Internet Speed 2022.csv')
+df2 = pd.read_csv('copy_data.csv')
+for index, row in df1.iterrows():
+    country = row['country']
+    data1 = row['mobile']
+    row2 = df2[df2['country'] == country]
+    if row2 is not None:
+        data2 = row2['mobile']
+        print(country, data2-data1)
+
+# # Apply the function to column 'A'
+# df['mobile'] = df['mobile'].apply(random_value)
+# df.to_csv('copy_data.csv')
+
+# df = df.sort_values('mobile', ascending=False)
+# df.hist(column='mobile', bins=10)
+# plt.show()
+# print(df.head(1))
 # print(df.loc[[10,20,30]])
 # new_country = {
 #     'country': 'Галактическая Республика', 
@@ -17,14 +35,14 @@ print(df.head(1))
 
 # print(df[df['mobile'].between(10, 15)])
 # print(df[(df['mobile'] > 10) & (df['mobile'] < 15)])
-print(df[df['country']=='Russia'])
+# print(df[df['country']=='Russia'])
 
-df = (df['mobile']
-      .sort_values(ascending=False)
-      .iloc[20:30]
-      .agg(['mean']))
+# df = (df['mobile']
+#       .sort_values(ascending=False)
+#       .iloc[20:30]
+#       .agg(['mean']))
 
-print(df)
+# print(df)
 # df = df[df['country'].str.len() == 5]
 # df = df.sort_values('broadband', ascending=True)
 # print(df.tail(1))
